5ad14806b99d8105f3d0e20524c8e579309189a8
Martin Sauczuk <martin.sauczuk@gmail.com>
2025-04-24 17:58:05 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,13 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import process from 'process';
 import { BREADRouter } from './Helpers';
 import { AutoController, PersonaController } from './Controllers';
 
 // Creamos nuestra app express
 const app = express();
 // Leemos el puerto de las variables de entorno, si no está, usamos uno por default
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 // Configuramos los plugins
 // Más adelante intentaremos entender mejor cómo funcionan estos plugins
@@ -23,6 +22,6 @@ app.use('/autos', BREADRouter(new AutoController()));
 /*************** FIN ENDPOINTS ******************/
 
 // Levantamos el servidor en el puerto que configuramos
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+app.listen(3000, () => {
+    console.log(`Server listening on port 3000`);
 });
